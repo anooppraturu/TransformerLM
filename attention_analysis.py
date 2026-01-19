d3db7fe0075a29c212bfce0e39ac9c846340a862
anooppraturu <anooppraturu@gmail.com>
2026-01-19 14:05:20 -0800

    collect statistics on control distribution for unbiased comparison

--- attention_analysis.py
+++ attention_analysis.py
@@ -162,6 +162,34 @@ def measure_conditional_attention_statistics(
 
     return stats, examples
 
+def get_control_statistics(examples, model, loader, device, conditional_tokens):
+    """
+    given collection of conditional examples, compute attention statistics on 
+    conditional_tokens with second to last token fixed to match statistics of
+    prev_token in examples
+    """
+    prev_controls, counts = examples['prev_token'].unique(return_counts=True)
+
+    control_indices = []
+
+    for tok, cnt in zip(prev_controls, counts):
+        ctrl_examples = collect_conditional_examples(
+            loader,
+            device,
+            conditional_tokens=conditional_tokens,
+            previous_tokens=[tok.item()],
+            num_samples=cnt.item()
+        )
+        control_indices.append(ctrl_examples["indices"])
+
+    control_indices = torch.cat(control_indices)
+
+    control_stats = measure_attention_on_examples(
+        model, loader, device, control_indices
+    )
+
+    return control_stats
+
 
 def phase_plots(phase_data):
     """
